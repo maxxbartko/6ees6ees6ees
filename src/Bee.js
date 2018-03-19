0dbb74bae1d526f623a5eb337e17b4ad5cf9947e
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-03-18 17:43:48 -0700

    All tests pass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor() {
+  	super();
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
 };
+
+// install bower
+// some kind of inheritance statment I need to look up
